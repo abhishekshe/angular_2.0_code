6105f1dba202a812ffeef026fea7e1899e645814
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2023-12-01 17:52:42 +0530

    form

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_2.0_codes';
+
+
+}
---
+++ src/app/templateform/templateform.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-templateform',
+  templateUrl: './templateform.component.html',
+  styleUrls: ['./templateform.component.css']
+})
+export class TemplateformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  Register(reData:NgForm){
+    console.log(reData.value);
+    
+  }
+  // Register(){}
+
+}
